8a38b18bc064cd175a632f56efc0a2bc0e33d96b
ISSOUMA Nafissath <issoumanafissath@gmail.com>
2026-01-12 15:48:47 +0100

    feat: configuration des secrets 256 car et suppression JWT_SECRET

--- src/config/env.js
+++ src/config/env.js
@@ -5,9 +5,10 @@ export const config = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
-  // Secret séparé pour les tokens email (Personne 2)
-  EMAIL_TOKEN_SECRET: process.env.EMAIL_TOKEN_SECRET || process.env.JWT_SECRET,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || process.env.JWT_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  EMAIL_TOKEN_SECRET: process.env.EMAIL_TOKEN_SECRET,
+
   // Configuration Email
   SMTP_HOST: process.env.SMTP_HOST,
   SMTP_PORT: process.env.SMTP_PORT,
@@ -17,4 +18,8 @@ export const config = {
   SMTP_FROM: process.env.SMTP_FROM,
   APP_NAME: process.env.APP_NAME || 'TP NodeJS',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3000',
+
+  // Configuration 2FA (Personne 4)
+  TWO_FACTOR_APP_NAME: process.env.TWO_FACTOR_APP_NAME || 'TP-NodeJS-Express',
+  TWO_FACTOR_ISSUER: process.env.TWO_FACTOR_ISSUER || 'TP-NodeJS-Express',
 };
